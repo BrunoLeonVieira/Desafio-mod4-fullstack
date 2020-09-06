b8becfab6a563f17234955a7cb1ec539f554ed0c
Bruno Cruz <brunolvcruz@gmail.com>
2020-09-06 19:05:22 -0300

    ajustes

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: "https://grades-app-leon.herokuapp.com/",
   })
 );
 
